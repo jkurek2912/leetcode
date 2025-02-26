c3b703cded80706f94586ee6071e1cbd606ccb77
jkurek2912 <kurek1@purdue.edu>
2025-02-26 14:43:29 -0500

    valid anagram

--- valid_anagram_242/valid_anagram_242.cpp
+++ valid_anagram_242/valid_anagram_242.cpp
@@ -11,3 +11,23 @@
 #include <chrono>
 #include <iomanip>
 
+class Solution {
+    public:
+        bool isAnagram(std::string s, std::string t) {
+            if (s.size() != t.size()) return false;
+            std::unordered_map<char, int> sMap;
+            std::unordered_map<char, int> tMap;
+            for (int i = 0; i < s.size(); i++) {
+                sMap[s[i]]++;
+            }
+            for (int i = 0; i < t.size(); i++) {
+                tMap[t[i]]++;
+            }
+            for (int i = 0; i < s.size(); i++) {
+                if (sMap[s[i]] != tMap[s[i]]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
